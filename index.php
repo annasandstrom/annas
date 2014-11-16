085d1adec72a5e832f811cbb3d2e20e7f3dc85db
Anna Sandstrom <anna_sandstrom1@hotmail.com>
2014-11-16 16:03:30 +0100

    1

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+</head>
+<body>
+
+
+
+<div class="jumbotron">
+  <h1>Hello, Anna!</h1>
+  <p>Välkommen till din sida!</p>
+  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+</div>
+
+
+</body>
+</html>
